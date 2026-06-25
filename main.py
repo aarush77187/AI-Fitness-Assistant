4846dca3df5717d3ddf23723250e6ae07544395b
Aarush Mudgil <aarushmudgil2021@gmail.com>
2026-06-25 11:40:20 +0530

    code updation

--- main.py
+++ main.py
@@ -1,39 +1,45 @@
 from FitnessAssistant import FitnessAssistant
-from storage import save_data
-p=[]
-try:
-    n=int(input("enter number of people : "))
-except ValueError:
-    print("Enter a valid number")
-    exit()
+from storage import save_data, view_history
 
-for i in range(n):
-    name=input("enter name of the person : ")
-    gender=input("enter person's gender : ")
-    try:
-        age=int(input("enter person's age : "))
-        height=float(input("enter height of the person : "))
-        weight=float(input("enter weight of the person : "))
+while True:
+    print("\n===== AI Fitness Assistant =====")
+    print("1. Add Person")
+    print("2. View History")
+    print("3. Exit")
+    choice = input("Enter your choice (1-3): ")
+    if choice == "1":
 
-    except ValueError:
-        print("Invalid Input!")
-        continue
-    activity=input("enter type of activity( Sedentary / Lightly Active / Moderately Active / Very Active) :  ")
-    try:
-        person = FitnessAssistant(
-            name,
-            gender,
-            age,
-            height,
-            weight,
-            activity
-        )
+        name=input("enter name of the person : \n")
+        gender=input("enter person's gender : \n")
+        try:
+            age=int(input("enter person's age : \n"))
+            height=float(input("enter height of the person : \n"))
+            weight=float(input("enter weight of the person :\n "))
 
-        p.append(person)
-        save_data(person)
+        except ValueError:
+            print("Invalid Input!\n")
+            continue
+        activity=input("enter type of activity( Sedentary / Lightly Active / Moderately Active / Very Active) :  \n")
+        try:
+            person = FitnessAssistant(
+                name,
+                gender,
+                age,
+                height,
+                weight,
+                activity
+            )
+            save_data(person)
+            person.display()
 
-    except ValueError as e:
-        print(e)
-for i in p:
-    i.display()
+        except ValueError as e:
+            print(f"Error: {e}")
+    elif choice == "2":
+        view_history()
+    elif choice == "3":
+        print("Exiting the program.")
+        break
+    else:
+        print("Invalid choice! Enter 1, 2, or 3.")
+
 
